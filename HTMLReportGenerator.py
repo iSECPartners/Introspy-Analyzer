c08b838b378e20e0b40fa0808550eb7d3a24fcbb
Alban D <nabla.c0d3@gmail.com>
2013-12-30 18:23:55 -0800

    Fix HTMLReportGenerator with the unified html folder

--- HTMLReportGenerator.py
+++ HTMLReportGenerator.py
@@ -11,9 +11,11 @@ class HTMLReportGenerator:
     FINDINGS_FILE_NAME =      'findings.js'
     API_GROUPS_FILE_NAME =    'apiGroups.js'
 
-    # TODO: merge the two templates
-    IOS_TEMPLATE_FOLDER = './html-ios'
-    ANDROID_TEMPLATE_FOLDER = './html-android'
+    # TODO: merge the two templates and get rid of this
+    HTML_TEMPLATE_PATH = os.path.abspath('html')
+    ANDROID_TEMPLATE = 'report-android.html'
+    IOS_TEMPLATE = 'report-ios.html'
+    FINAL_TEMPLATE = 'report.html'
 
 
     def __init__(self, analyzedDB, androidDb):
@@ -22,11 +24,22 @@ class HTMLReportGenerator:
 
 
     def write_report_to_directory(self, outDir):
-        # Copy the template
+
+        # Copy the HTML template
+        shutil.copytree(os.path.abspath(self.HTML_TEMPLATE_PATH), outDir)
+
+
         if self.androidDb:
-            shutil.copytree(self.ANDROID_TEMPLATE_FOLDER, outDir)
+            shutil.move(os.path.join(outDir, self.ANDROID_TEMPLATE),
+                        os.path.join(outDir, self.FINAL_TEMPLATE))
+            # Remove the wrong template file
+            os.remove(os.path.join(outDir, self.IOS_TEMPLATE))
         else:
-            shutil.copytree(self.IOS_TEMPLATE_FOLDER, outDir)
+            shutil.move(os.path.join(outDir, self.IOS_TEMPLATE),
+                        os.path.join(outDir, self.FINAL_TEMPLATE))
+            # Remove the wrong template file
+            os.remove(os.path.join(outDir, self.ANDROID_TEMPLATE))
+
 
         # Copy the DB file
         shutil.copy(self.analyzedDB.dbPath, outDir)
